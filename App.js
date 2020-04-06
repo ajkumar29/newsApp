6146b819015287a555bb63ec7c031682e991796a
AJ <ajanthan29894@gmail.com>
2020-04-06 22:40:02 +0100

    implemented simple news app

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, FlatList } from "react-native";
+import Article from "./src/components/Article";
 
 export default function App() {
+  const url =
+    "https://newsapi.org/v2/top-headlines?country=us&apiKey=896b2115870e4b138219932078a82bc5";
+
+  const [articles, setArticles] = useState([]);
+  const [refreshing, setRefreshing] = useState(true);
+
+  async function fetchArticles() {
+    const res = await fetch(url);
+    res
+      .json()
+      .then((res) => {
+        setArticles(res.articles);
+        setRefreshing(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setRefreshing(false);
+      });
+  }
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const handleRefresh = () => {
+    setRefreshing(true);
+    fetchArticles();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! LOOL</Text>
-    </View>
+    <FlatList
+      data={articles}
+      renderItem={({ item }) => {
+        return <Article article={item} />;
+      }}
+      keyExtractor={(item) => item.url}
+      refreshing={refreshing}
+      onRefresh={handleRefresh}
+    />
+
+    // <Text>{JSON.stringify(articles)}</Text>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
